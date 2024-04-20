d22363c3ffe5632055c4f11cdc03754eb3bd0413
Jimm1337 <263970@student.pwr.edu.pl>
2024-04-20 17:27:15 +0200

    [Add] Add: Initial commit, project setup.

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <raylib.h>
+
+int main() {
+  InitWindow(800, 600, "Hello, World!");
+
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("Hello, World!", 10, 10, 20, DARKGRAY);
+    EndDrawing();
+  }
+
+  CloseWindow();
+
+  return 0;
+}
